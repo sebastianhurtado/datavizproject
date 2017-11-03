338f761058152d72e2e264002d494a9922395f7f
sebastianhurtado <32179227+sebastianhurtado@users.noreply.github.com>
2017-11-02 20:47:19 -0400

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -1,30 +1,113 @@
+// Bubble chart using D3
 var diameter = 420;
-var colors = ['#2AA4A9', '#57B28D','#FBAE4B', '#F16045','#6f9364', '#efcbdb', '#7082c1'];
+var colors = ['#2AA4A9', '#57B28D','#FBAE4B', '#F16045', '#D12258', '#5E4E73', '#C2B49B', '#734743', '#80A464', '#435773'];
 var color = d3.scale.category10().range(colors);
 var bubble = d3.layout.pack()
     .sort(null)
-    .size([diameter * 1.4, diameter])
-    .padding(10);
+    .size([diameter * 1.4, diameter]) //size of the bubble chart
+    .padding(1);
 var svg = d3.select("body").select("#svg_bubble");
-var tooltip = d3.select("body")
+var tooltip = d3.select("body") //set the tooltip
     .append("div")
     .style("position", "absolute")
-    .style("z-index", "100")
+    .style("z-index", "10")
     .style("visibility", "hidden")
-    .style("font", "30px PT Calibri")
+    .style("color", "black")
+    .style("background-color", "rgba(0, 0, 0, 0)")
+    .style("border-radius", "6px")
+    .style("font", "40px PT Sans")
     .text("tooltip");
-var habbits = [
-    {category: "Funding", alt: "Funding - 100,291 entries", value: 2.0},
-    {category: "SETI", alt: "SETI - 214 entries", value: 0.5},
-    {category: "Exoplanets", alt: "Exoplanets - 112 entries", value: 0.1},
-    {category: "Funding & SETI", alt: "Funding & SETI - 13 entries", value: 0.08},
-    {category: "Funding & Exoplanets", alt: "Funding & Exoplanets - 9 entries", value: 0.05},
-    {category: "Exoplanets & SETI", alt: "Exoplanets & SETI - 8 entries", value: 0.05},
-    {category: "Funding, Exoplanets & SETI", alt: "Funding,Exoplanets & SETI - ????", value: 0.05},
+
+// 1. Modify the dataset
+var Taylors = [
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Taylor Ham", name: "6.73%", alt: "yummm", value: 0.6729},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Taylor Swift", name: "10.39%", alt: "♪♫♬", value: 1.038},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
+    {category: "Other Taylor", alt: "?", value: 0.1},
 ];
 
+// 2. Change the color of the node when hovered over
 var node = svg.selectAll(".node")
-    .data(bubble.nodes({children:habbits}).filter(function(d) { return !d.children; }))
+    .data(bubble.nodes({children:Taylors}).filter(function(d) { return !d.children; }))
   .enter().append("g")
     .attr("class", "node")
     .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
@@ -41,11 +124,53 @@ node.append("circle")
     .on("mouseout", function(){
         return tooltip.style("visibility", "hidden");
     });
+
+// 3. Change the font size to be based on node radius
 node.append("text")
     .attr("dy", ".3em")
     .style("text-anchor", "middle")
     .style("pointer-events", "none")
     .text(function(d) { return d.name; });
+
+// Legend to show all the categories in the dataset
+var legend = d3.select("#bubble").append("svg").attr("id", "legend");
+legend.append("rect").attr("id", "legend1")
+    .attr("x", "120").attr("y", "10").attr("width", "20").attr("height", "20").attr("fill", color("Taylor Swift"));
+legend.append("text").attr("x", "145").attr("y", "24").attr("font-size", "12px").text("Taylor Swift");
+legend.append("rect").attr("id", "legend2")
+    .attr("x", "250").attr("y", "10").attr("width", "20").attr("height", "20").attr("fill", color("Taylor Ham"));
+legend.append("text").attr("x", "275").attr("y", "24").attr("font-size", "12px").text("Taylor Ham");
+legend.append("rect").attr("id", "legend3")
+    .attr("x", "370").attr("y", "10").attr("width", "20").attr("height", "20").attr("fill", color("Other Taylor"));
+legend.append("text").attr("x", "395").attr("y", "24").attr("font-size", "12px").text("Other Taylor");
+
+// Function to shuffle the colors in the bubble chart
+function shuffle_colors(){
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+        return array;
+    }
+    shuffle(colors);
+    color = d3.scale.category10().range(colors);
+    svg.selectAll(".node")
+       .select("circle")
+       .style("fill", function(d) { return color(d.category); });
+    legend.select("#legend1").attr("fill", color("Taylor Swift"));
+    legend.select("#legend2").attr("fill", color("Taylor Ham"));
+    legend.select("#legend3").attr("fill", color("Other Taylor"));
+}
+
+// Function to filter out the nodes based on the user inputs
 function filter(category){
     d3.select(".dropbtn").select("#cat").text(category);
     switch(category){
