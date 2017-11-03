57cf83d03eb2b16c78fb02638fa0e941ef79300a
sebastianhurtado <32179227+sebastianhurtado@users.noreply.github.com>
2017-11-02 22:19:00 -0400

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -9,7 +9,7 @@ var svg = d3.select("body").select("#svg_bubble");
 var tooltip = d3.select("body")
     .append("div")
     .style("position", "absolute")
-    .style("z-index", "10")
+    .style("z-index", "1")
     .style("visibility", "hidden")
     .style("font", "30px PT Calibri")
     .text("tooltip");
